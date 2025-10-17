9a3a86cffcc0b0b76f313ac580a0674fcf6e436e
Staish69Batareyka <anastaish.batareyka@yandex.ru>
2025-10-17 15:53:34 +0300

    feat(bot.py): Добавить многострочное сообщение для команды start

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 import telebot
+import datetime
+import threading # для ассинхронности
 
 # Токен для доступа к боту
 TOKEN = '8308029471:AAHrakR63QGHglWBcUHMRQiPZiuCmkh1_-k'
@@ -10,19 +12,23 @@ bot = telebot.TeleBot(TOKEN)
 # Создаем обработчики команд
 @bot.message_handler(commands=['start'])
 def send_message(message):
-    # Принимаем /start , возвращаем сообщение
-    bot.send_message(message.chat.id, 'Привет ^^/ . Я - твое эхо. Скажи что-нибудь - я повторю')
 
+    # Формат текста именно такой, потому что иначе telegram
+    # вставляет лишние пробелы в многострочное сообщение
+    welcome_text = """
+    Привет *★,°*:.☆(￣▽￣)/$:*.°★* 。*★.
+    
+Я - бот Remanda. Моя задача - напоминать тебе о всяких вещах, о которых стоило бы помнить, но ты обязательно забудешь 
+    
+ヽ(￣ω￣(￣ω￣〃)ゝ
+    
+Не волнуйся, браток, я готов тебе с этим помочь.
+    
+Введи /reminder ,  чтобы создать напоминалку
+    """
+    # Принимаем /start, возвращаем сообщение
+    bot.send_message(message.chat.id, welcome_text)
 
-# Обработчик любого сообщения, которое прислали, чтобы повторить его
-
-# Параметр func= создает условия для выполнения,
-# lambda обозначает, что мы создаем маленькую функцию в 1 строку
-# message: True - параметр функции и возвращаемое значение
-
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
 
 if __name__ == '__main__':
     print('Запуск успешен')
