7a281305d8746f50ea78da0460be01ad1be942f5
Staish69Batareyka <anastaish.batareyka@yandex.ru>
2025-10-23 02:45:44 +0300

    feat(pro_bot.py): Инициализировать все глобальные переменные, которые потенциально необхтдимы
    Добавила класс для обозначения типа чата, словарь для определения временных интервалов (буду использовать с функцией поиска подстроки), хранилище данных для пользователей и групп

---
+++ pro_bot.py
@@ -0,0 +1,35 @@
+import telebot
+import threading
+import datetime
+import re # Для регулярных выражений
+import time # Для низкоуровневой работы со временем
+from enum import Enum # Для работы с перечислениями = именованными константами
+
+from bot import user_data
+
+# (просто улучшает читабельность, синтаксический сахар)
+
+TOKEN = '8308029471:AAHrakR63QGHglWBcUHMRQiPZiuCmkh1_-k'
+bot = telebot.TeleBot(TOKEN)
+
+
+# Обозначение типа чата
+class ChatType(Enum):
+    PRIVATE = 'private',
+    GROUP = 'group'
+
+
+# Словарь для определения временных интервалов
+TIME_UNIT = {
+    'cек': 1,
+    'мин': 60,
+    'час': 3600,
+    ('дн', 'день'): 86400,
+    'нед': 604800,
+    'мес': 2592000,
+}
+
+# Хранилище данных (пока так. Потом мб надо будет подключить какую-нить базу данных)
+users_data = {}
+group_data = {}
+
