081d2d21e05ab004bae41d37f1811e6b54561581
Staish69Batareyka <anastaish.batareyka@yandex.ru>
2025-10-23 19:46:48 +0300

    feat(pro_bot.py): Добавить каркасы функций для обработки естественного языка пользователя
    Добавила каркасы фукнций handler_language и parse_reminder_text

--- pro_bot.py
+++ pro_bot.py
@@ -1,4 +1,7 @@
 import telebot
+from telebot.types import (KeyboardButton,  # Класс для создания кнопки
+                           ReplyKeyboardMarkup, # Класс для создания клавиатуры
+                           ReplyKeyboardRemove) # Класс для скрытия клавиатуры
 import threading
 import datetime
 import re # Для регулярных выражений
@@ -34,11 +37,22 @@ users_data = {}
 group_data = {}
 
 
+def create_menu():
+    menu = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    buttons = [
+        KeyboardButton('Создать напоминалку'),
+        KeyboardButton('Мои напоминалки'),
+        KeyboardButton(''),
+        KeyboardButton(''),
+    ]
+    menu.add(*buttons[:2])
+    menu.add(*buttons[2:])
+    return menu
 
 # Функция /start
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message, '''
+    bot_start_message = '''
     Привет *★,°*:.☆(￣▽￣)/$:*.°★* 。*★.
 
 Я - бот Remanda. Моя задача - напоминать тебе о всяких вещах, о которых стоило бы помнить, но ты обязательно забудешь 
@@ -47,9 +61,35 @@ def start_message(message):
 
 Не волнуйся, браток, я готов тебе с этим помочь.
 
-Введи /reminder ,  чтобы создать напоминалку
-    ''')
+Ещё я достаточно умный. Так что можешь написать в любом формате сообщение о напоминании. Что-то вроде "Завтра в 14:00 встреча" или "Мне нужно лечь спать в 8 утра". Я тебя пойму.
 
+Используй кнопки и оцени все мои функции.
+    '''
+    bot.send_message(message.chat.id, bot_start_message, reply_markup=create_menu())
+
+
+@bot.message_handler(func=lambda message: message.text == 'Создать напоминалку')
+def create_reminder(message):
+    bot.send_message(message.chat.id, 'Чего ты хочешь от меня?')
+
+
+# Парсинг сообщения и извлечение названия и времени напоминания
+def parse_reminder_text(text):
+    to_lowercase = text.lower()
+
+    # Относительное время
+
+    # Абсолютное время
+
+    return
+
+
+# Обработка естественного языка пользователя
+@bot.message_handler(func=lambda message: True)
+def handler_language(message):
+    text = message.text
+    chat_id = message.chat.id
+    reminder_text, reminder_time = True
 
 
 
